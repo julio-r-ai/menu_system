cb965a8a25e55e5e83df4f659f7408a3838890ac
Julio Cesar <julioreis044@gmail.com>
2023-12-19 23:47:26 -0300

    Comitando

--- resources/views/cerveja.blade.php
+++ resources/views/cerveja.blade.php
@@ -7,29 +7,22 @@
 @section('content')
 
 <div class="general" onload="showLoading()">
-    
-    {{-- <h1>Cervejas</h1> --}}
     @foreach ($products as $product)
-
-        @if ($product->category === 'Servejas')   
-            <div class="card">
-                <div class="card-inter">
-                    <div>
-                        <img src="img/products/{{$product->img}}" alt="imagem">
-                    </div>
-                    <div>
-                        <h2 id="descriH2">{{$product->description}}</h2>
-                        <h3 id="descriH3">R$ {{$product->price}}</h3>                        
-                    </div>
+    @if ($product->category === 'Cerveja')
+        <div class="card">
+            <div class="card-inter">
+            <div>
+                    <img src="img/products/{{$product->img}}" alt="imagem">
+            </div>
+                <div>
+                    <h2 id="descriH2">{{$product->description}}</h2>
+                    <h2 id="descriH3">R$ {{$product->price}}</h2>
                 </div>
-                <div >
-                    <a href="/showProduto/{{ $product->id }}"><button id="buttonVer">Ver</button></a>
-                </div>
-            </div> 
-        @endif
-    @endforeach 
-
-  
-
+            </div>
+            <div>
+                <a href="/showProduto/{{ $product->id }}"><button id="buttonVer">Ver</button></a>
+            </div>
+        </div> 
+    @endif
+@endforeach 
 </div>  
-
